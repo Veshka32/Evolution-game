cf3f33e0dfc73d0000b835f0c4c925fd7d34525e
Nataly Veshka <32inter@gmail.com>
2018-04-29 14:16:26 +0300

    connect to database with connection pool in glassfish

---
+++ src/main/java/services/dataBaseService/DBService.java
@@ -0,0 +1,58 @@
+package services.dataBaseService;
+
+import java.sql.*;
+
+public class DBService {
+
+    public DBService(){}
+
+    private static final JDBCService jdbcService = DataAccessFactory.getJDBCUtils();
+
+    public static void getData() throws Exception {
+        //Map<Integer, String> data = new HashMap<Integer, String>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = jdbcService.getConnection();
+            //Class.forName("org.h2.Driver");
+            //connection=DriverManager.getConnection("jdbc:h2:C:/Users/stas/Documents/evo/h2base", "admin", "admin");
+            System.out.println(connection.isValid(10)); //check connection is open
+            statement=connection.createStatement();
+            String sql="DROP TABLE IF EXISTS Users";
+            statement.executeUpdate(sql);
+            sql="CREATE TABLE IF NOT EXISTS Users (id INTEGER not NULL, login VARCHAR(255), password VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id))";
+            statement.executeUpdate(sql);
+            sql="INSERT INTO Users VALUES(1, 'test','test','email')";
+            statement.executeUpdate(sql);
+            sql="INSERT INTO Users VALUES(2, 'poppy','test','email')";
+            statement.executeUpdate(sql);
+            sql="SHOW TABLES";
+            resultSet=statement.executeQuery(sql);
+            sql="SHOW COLUMNS FROM Users";
+            resultSet=statement.executeQuery(sql);
+            sql="SELECT login FROM Users";
+            resultSet=statement.executeQuery(sql);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("login"));}
+
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+                System.out.println("Is resultset clodsed"+resultSet.isClosed());
+            }
+
+            if (statement != null) {
+                statement.close();
+                System.out.println("Is statement clodsed"+statement.isClosed());
+            }
+
+            if (connection != null) {
+                connection.close();
+                System.out.println("Is conn clodsed"+connection.isClosed());
+            }
+        }
+    }
+    }
+
+
---
+++ src/main/java/services/dataBaseService/JDBCService.java
@@ -0,0 +1,35 @@
+package services.dataBaseService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class JDBCService{
+
+    private DataSource dataSource;
+
+    public JDBCService() {
+    }
+
+    public void init(String dataSourceName) {
+        try {
+            InitialContext initContext = new InitialContext();
+            dataSource = (DataSource) initContext.lookup(dataSourceName);
+        } catch (NamingException e) {
+        }
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        if (dataSource == null) {
+            throw new SQLException("DataSource is null.");
+        }
+        //org.h2.Driver.load();
+        Class.forName("org.h2.Driver");
+        return dataSource.getConnection();
+    }
+
+}
