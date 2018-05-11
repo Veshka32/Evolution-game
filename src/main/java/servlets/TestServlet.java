2183af06bc076fcb9484e1e09a4131cd978541c0
Nataly Veshka <32inter@gmail.com>
2018-05-11 18:32:53 +0300

    add test for persistence 2

--- src/main/java/servlets/TestServlet.java
+++ src/main/java/servlets/TestServlet.java
@@ -4,16 +4,19 @@ import services.dataBaseService.Test;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/testServlet")
 public class TestServlet extends HttpServlet {
     @EJB
     private Test testBean;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         testBean.addUser("testUser");
+        System.out.println("user added");
     }
 }
